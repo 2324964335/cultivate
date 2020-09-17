467902e6438026aee82aaa1b721434e893d4d6ea
qweqwqsqwwew <2430711426@qq.com>
2020-09-17 10:55:35 +0800

    fix

--- lib/utils/screen_adaper.dart
+++ lib/utils/screen_adaper.dart
@@ -1,6 +1,45 @@
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'dart:io';
+//import 'dart:io';
 /// 直接通过`类名称`访问里面的方法，方法为 静态方法
+const bool kIsWeb = identical(0, 0.0);
+class ScreenAdaper {
+  static init(context){
+    /// 设计稿 --- 宽/高
+    ScreenUtil.init(context,width: 750,height: 1334);
+  }
+  static sp(double value){
+      return ScreenUtil().setSp(value, allowFontScalingSelf: true);  /// 获取 计算后的字体
+
+  }
+  static height(double value){
+
+      return ScreenUtil().setHeight(value);
+
+      /// 获取 计算后的高度
+  }
+  static width(double value){
+
+      return ScreenUtil().setWidth(value);    /// 获取 计算后的宽度
+  }
+  static screenHeight(){
+
+      return ScreenUtil.screenHeight;
+
+      /// 获取 计算后的屏幕高度
+  }
+  static screenWidth(){
+    return  ScreenUtil.screenWidth;  /// 获取 计算后的屏幕高度
+  }
+
+}
+
+/*
+*
+*
+* import 'package:flutter_screenutil/flutter_screenutil.dart';
+//import 'dart:io';
+/// 直接通过`类名称`访问里面的方法，方法为 静态方法
+const bool kIsWeb = identical(0, 0.0);
 class ScreenAdaper {
   static init(context){
     /// 设计稿 --- 宽/高
@@ -15,6 +54,7 @@ class ScreenAdaper {
     else{
       return ScreenUtil().setSp(value, allowFontScalingSelf: true);  /// 获取 计算后的字体
     }
+
   }
   static height(double value){
     if(Platform.isMacOS){
@@ -45,3 +85,4 @@ class ScreenAdaper {
   }
 
 }
+* */
